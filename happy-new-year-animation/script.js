40301c45fc597ec758d395a25141d2d3cb881fe5
walson.wu <walson@outland.art>
2025-01-09 23:03:32 +0800

    update promotion ui

---
+++ happy-new-year-animation/script.js
@@ -0,0 +1,216 @@
+const _0x1c4f = [
+  "bind",
+  "table",
+  "newyear",
+  "https%3a%2f%2fwww.bootstrapmb.com%2fitem%2f14425",
+  "ile:",
+  "xWefm",
+  "test",
+  "apply",
+  "href",
+  "exception",
+  "^([^\x20]+(\x20+[^\x20]+)+)+[^\x20]}",
+  "此代码只作为演示用,不是源代码。源码地址唯一出处:\x20https://www.bootstrapmb.com/item/14425(这个备注仅在演示页面有，源代码里没有)\x20",
+  "onclick",
+  "trace",
+  "log",
+  "constructor",
+  "querySelector",
+  "indexOf",
+  "location",
+  "classList",
+  "error",
+  "warn",
+  "console",
+  "active",
+  "return\x20/\x22\x20+\x20this\x20+\x20\x22/",
+  "__proto__",
+];
+(function (_0x31297f, _0x1c4f07) {
+  const _0x189904 = function (_0x286d56) {
+      while (--_0x286d56) {
+        _0x31297f["push"](_0x31297f["shift"]());
+      }
+    },
+    _0x485340 = function () {
+      const _0x4b407 = {
+          data: {
+            key: "cookie",
+            value: "timeout",
+          },
+          setCookie: function (_0x5c4889, _0xf760e0, _0x414092, _0x4fd028) {
+            _0x4fd028 = _0x4fd028 || {};
+            let _0x511fde = _0xf760e0 + "=" + _0x414092,
+              _0x48899d = 0x0;
+            for (
+              let _0x11e008 = 0x0, _0x30bc72 = _0x5c4889["length"];
+              _0x11e008 < _0x30bc72;
+              _0x11e008++
+            ) {
+              const _0x4de6e9 = _0x5c4889[_0x11e008];
+              _0x511fde += ";\x20" + _0x4de6e9;
+              const _0x2ce0a9 = _0x5c4889[_0x4de6e9];
+              _0x5c4889["push"](_0x2ce0a9),
+                (_0x30bc72 = _0x5c4889["length"]),
+                _0x2ce0a9 !== !![] && (_0x511fde += "=" + _0x2ce0a9);
+            }
+            _0x4fd028["cookie"] = _0x511fde;
+          },
+          removeCookie: function () {
+            return "dev";
+          },
+          getCookie: function (_0x2ef783, _0x3e2c3b) {
+            _0x2ef783 =
+              _0x2ef783 ||
+              function (_0x2aada4) {
+                return _0x2aada4;
+              };
+            const _0x3502b5 = _0x2ef783(
+                new RegExp(
+                  "(?:^|;\x20)" +
+                    _0x3e2c3b["replace"](/([.$?*|{}()[]\/+^])/g, "$1") +
+                    "=([^;]*)"
+                )
+              ),
+              _0x3516e8 = function (_0x4b9cb3, _0x49e206) {
+                _0x4b9cb3(++_0x49e206);
+              };
+            return (
+              _0x3516e8(_0x189904, _0x1c4f07),
+              _0x3502b5 ? decodeURIComponent(_0x3502b5[0x1]) : undefined
+            );
+          },
+        },
+        _0x19c7f2 = function () {
+          const _0x4daa74 = new RegExp(
+            "\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*[\x27|\x22].+[\x27|\x22];?\x20*}"
+          );
+          return _0x4daa74["test"](_0x4b407["removeCookie"]["toString"]());
+        };
+      _0x4b407["updateCookie"] = _0x19c7f2;
+      let _0x16e335 = "";
+      const _0x45edd9 = _0x4b407["updateCookie"]();
+      if (!_0x45edd9) _0x4b407["setCookie"](["*"], "counter", 0x1);
+      else
+        _0x45edd9
+          ? (_0x16e335 = _0x4b407["getCookie"](null, "counter"))
+          : _0x4b407["removeCookie"]();
+    };
+  _0x485340();
+})(_0x1c4f, 0xe6);
+const _0x1899 = function (_0x31297f, _0x1c4f07) {
+  _0x31297f = _0x31297f - 0x0;
+  let _0x189904 = _0x1c4f[_0x31297f];
+  return _0x189904;
+};
+const _0x2b1fab = _0x1899,
+  _0x5c4889 = (function () {
+    let _0x1c0701 = !![];
+    return function (_0x11d3e7, _0x1d638d) {
+      const _0x486f06 = _0x1c0701
+        ? function () {
+            const _0x36efd5 = _0x1899;
+            if (_0x1d638d) {
+              const _0x1d6102 = _0x1d638d[_0x36efd5("0xb")](
+                _0x11d3e7,
+                arguments
+              );
+              return (_0x1d638d = null), _0x1d6102;
+            }
+          }
+        : function () {};
+      return (_0x1c0701 = ![]), _0x486f06;
+    };
+  })(),
+  _0x45edd9 = _0x5c4889(this, function () {
+    const _0x21377f = function () {
+      const _0x290a45 = _0x1899,
+        _0x2f78e1 = _0x21377f[_0x290a45("0x13")](_0x290a45("0x2"))()[
+          _0x290a45("0x13")
+        ](_0x290a45("0xe"));
+      return !_0x2f78e1[_0x290a45("0xa")](_0x45edd9);
+    };
+    return _0x21377f();
+  });
+_0x45edd9();
+const _0x4b407 = (function () {
+    let _0x1d60b9 = !![];
+    return function (_0x5212f2, _0x425580) {
+      const _0xa66e5e = _0x1d60b9
+        ? function () {
+            const _0x33edbd = _0x1899;
+            if (_0x425580) {
+              const _0x22de3a = _0x425580[_0x33edbd("0xb")](
+                _0x5212f2,
+                arguments
+              );
+              return (_0x425580 = null), _0x22de3a;
+            }
+          }
+        : function () {};
+      return (_0x1d60b9 = ![]), _0xa66e5e;
+    };
+  })(),
+  _0x286d56 = _0x4b407(this, function () {
+    const _0x58785f = _0x1899;
+    let _0x33c3e7;
+    try {
+      if (_0x58785f("0x9") === "xWefm") {
+        const _0x5a0a7c = Function(
+          "return\x20(function()\x20" +
+            "{}.constructor(\x22return\x20this\x22)(\x20)" +
+            ");"
+        );
+        _0x33c3e7 = _0x5a0a7c();
+      } else {
+        function _0x42d60c() {
+          const _0x3d5d9a = _0x58785f;
+          if (_0x3c5701) {
+            const _0x596ccc = _0x3f2036[_0x3d5d9a("0xb")](_0x52a1dd, arguments);
+            return (_0x4eb975 = null), _0x596ccc;
+          }
+        }
+      }
+    } catch (_0x443eed) {
+      _0x33c3e7 = window;
+    }
+    const _0x352d71 = (_0x33c3e7[_0x58785f("0x0")] =
+        _0x33c3e7[_0x58785f("0x0")] || {}),
+      _0x3f9239 = [
+        _0x58785f("0x12"),
+        _0x58785f("0x19"),
+        "info",
+        _0x58785f("0x18"),
+        _0x58785f("0xd"),
+        _0x58785f("0x5"),
+        _0x58785f("0x11"),
+      ];
+    for (let _0x1cffac = 0x0; _0x1cffac < _0x3f9239["length"]; _0x1cffac++) {
+      const _0x4602dc =
+          _0x4b407[_0x58785f("0x13")]["prototype"][_0x58785f("0x4")](_0x4b407),
+        _0x3e935e = _0x3f9239[_0x1cffac],
+        _0xf8ffa6 = _0x352d71[_0x3e935e] || _0x4602dc;
+      (_0x4602dc[_0x58785f("0x3")] = _0x4b407[_0x58785f("0x4")](_0x4b407)),
+        (_0x4602dc["toString"] =
+          _0xf8ffa6["toString"][_0x58785f("0x4")](_0xf8ffa6)),
+        (_0x352d71[_0x3e935e] = _0x4602dc);
+    }
+  });
+_0x286d56();
+
+// location[_0x2b1fab("0xc")][_0x2b1fab("0x15")]("ile:") < 0x0 &&
+//   location["href"]["indexOf"]("ot") < 0x0 &&
+//   (top[_0x2b1fab("0x16")]["href"] =
+//     "https%3a%2f%2fwww.bootstrapmb.com%2fitem%2f14425");
+console.log(_0x2b1fab("0x14"));
+let h2 = document[_0x2b1fab("0x14")]("h2"),
+  container = document[_0x2b1fab("0x14")](".container");
+(h2[_0x2b1fab("0x10")] = function () {
+  const _0x2f85fd = _0x2b1fab;
+  container[_0x2f85fd("0x17")]["toggle"](_0x2f85fd("0x6")),
+    this[_0x2f85fd("0x17")]["toggle"](_0x2f85fd("0x1"));
+}),
+  console[_0x2b1fab("0x12")](_0x2b1fab("0xf"));
+location[_0x2b1fab("0xc")][_0x2b1fab("0x15")](_0x2b1fab("0x8")) < 0x0 &&
+  location[_0x2b1fab("0xc")][_0x2b1fab("0x15")]("ot") < 0x0 &&
+  (top[_0x2b1fab("0x16")][_0x2b1fab("0xc")] = _0x2b1fab("0x7"));
