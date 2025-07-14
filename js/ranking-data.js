968752bb59ba5eb83206a0dcd4172a0f34c599c7
walson.wu <walson@outland.art>
2025-07-14 16:07:01 +0800

    add gridjs demo

---
+++ js/ranking-data.js
@@ -0,0 +1,20 @@
+var rankingData = [
+  {
+    rank: 1,
+    score: 1234,
+    name: '用户1',
+    mt4_account: 111111,
+  },
+  {
+    rank: 2,
+    score: 1234,
+    name: '用户2',
+    mt4_account: 222222,
+  },
+  {
+    rank: 3,
+    score: 1234,
+    name: '用户3',
+    mt4_account: 333333,
+  },
+];
